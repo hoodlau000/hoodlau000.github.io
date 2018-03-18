d446419cdc231b5cbe932e63bf2f1b6eae90f4bf
hoodlau000 <35362766+hoodlau000@users.noreply.github.com>
2018-03-17 18:51:02 -0700

    Fully updated website

---
+++ assignments/weathersite/js/wu-weather-spring.js
@@ -0,0 +1,15 @@
+var weatherObject = new XMLHttpRequest();
+
+weatherObject.open('GET','//api.wunderground.com/api/bffd108e29bbe840/conditions/q/OR/Springfield.json', true);
+
+weatherObject.send();
+
+weatherObject.onload = function() {
+	var weatherInfo = JSON.parse(weatherObject.responseText);
+	console.log(weatherInfo);
+
+document.getElementById('currentWeather').innerHTML = weatherInfo.current_observation.weather;
+document.getElementById('currentTemp').innerHTML = weatherInfo.current_observation.temp_f;
+document.getElementById('currentWS').innerHTML = weatherInfo.current_observation.wind_gust_mph;
+document.getElementById('weatherIcon').src = weatherInfo.current_observation.icon_url;
+}
